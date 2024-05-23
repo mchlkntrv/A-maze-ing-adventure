9832a4343e4b0f7d10ec0b035632e6e1476451b0
MICHAELA\micha <michaela.kantorova@gmail.com>
2024-05-23 17:45:04 +0200

    level manager done

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("You have finished all levels!");
+        }
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
@@ -19,7 +20,10 @@ public class PlayerController : MonoBehaviour
     public GameObject startTrigger;
     public GameObject finishTrigger;
 
-    public TMP_Text finishScoreText;
+    public TMP_Text levelScoreText;
+    public TMP_Text bestScoreText;
+    public TMP_Text levelTimeText;
+    public TMP_Text bestTimeText;
     public GameObject finishPopUp;
 
     public TMP_Text timerText;
@@ -27,6 +31,12 @@ public class PlayerController : MonoBehaviour
     private bool counting = false;
 
     private bool reachedFinish = false;
+
+    private string levelKey;
+    private float bestTime;
+    private int bestScore;
+
+    public Button nextLevelButton;
 
     private void Start()
     {
@@ -36,6 +46,12 @@ public class PlayerController : MonoBehaviour
         scoreText.text = "Score: 0";
         timerText.text = "Timer: 00:00";
         finishPopUp.SetActive(false);
+
+        levelKey = "Level_" + SceneManager.GetActiveScene().name;
+        bestTime = PlayerPrefs.GetFloat(levelKey + "_BestTime", float.MaxValue);
+        bestScore = PlayerPrefs.GetInt(levelKey + "_BestScore", 0);
+
+        nextLevelButton.onClick.AddListener(OnNextLevelButton);
     }
 
     void Update()
@@ -160,18 +176,70 @@ public class PlayerController : MonoBehaviour
         speed /= multiplier;
     }
 
+
+    private void SaveBestTime()
+    {
+        if (timer < bestTime)
+        {
+            bestTime = timer;
+            PlayerPrefs.SetFloat(levelKey, bestTime);
+            PlayerPrefs.Save();
+            Debug.Log("New best time: " + bestTime);
+        }
+    }
+
+    private void SaveBestScore()
+    {
+        if (score > bestScore)
+        {
+            bestScore = score;
+            PlayerPrefs.SetInt(levelKey + "_BestScore", bestScore);
+            PlayerPrefs.Save();
+            Debug.Log("New best score: " + bestScore);
+        }
+    }
+
+
+    private void ShowFinishPopup()
+    {
+        finishPopUp.SetActive(true);
+        levelScoreText.text = "Score: " + score.ToString();
+        bestScoreText.text = "Best score: " + bestScore.ToString();
+
+        int minutes = Mathf.FloorToInt(timer / 60f);
+        int seconds = Mathf.FloorToInt(timer % 60f);
+        string timeString = string.Format("{0:00}:{1:00}", minutes, seconds);
+        levelTimeText.text = "Time: " + timeString;
+
+        minutes = Mathf.FloorToInt(bestTime / 60f);
+        seconds = Mathf.FloorToInt(bestTime % 60f);
+        timeString = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        bestTimeText.text = "Best time: " + timeString;
+
+    }
+
+    private void OnNextLevelButton()
+    {
+        LevelManager.instance.LoadNextLevel();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject == finishTrigger)
         {
-            finishPopUp.SetActive(true);
-            finishScoreText.text = "Final score: " + score.ToString();
             reachedFinish = true;
             isMoving = false;
             StopTimer();
+
+            SaveBestTime();
+            SaveBestScore();
+            ShowFinishPopup();
+
             Debug.Log("Finish reached!");
         }
     }
+
 
     private void OnTriggerExit2D(Collider2D other)
     {
